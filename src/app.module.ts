43aba756d7ea323259fea6a239a2d3335d0d4aea
shivamt <shivamt@wiznox.com>
2023-07-27 15:16:58 +0530

    10th test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { moduleArray } from './moduleArray';
 import { PrismaService } from '@/prisma.service';
 import { ErrorFunction } from '@/error.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [...moduleArray],
-  controllers: [],
-  providers: [PrismaService, ErrorFunction],
+  controllers: [AppController],
+  providers: [PrismaService, ErrorFunction, AppService],
+  exports:[PrismaService]
 })
 export class AppModule {}
