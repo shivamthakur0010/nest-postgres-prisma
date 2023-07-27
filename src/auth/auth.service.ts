241a90a3cdff79260e36c5a5d11ebbeb88bf9100
shivamt <shivamt@wiznox.com>
2023-07-27 15:34:43 +0530

    12th test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from '@/users/users.service';
+import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from '@/prisma.service';
-import { jwtConstants } from '@/auth/constants';
-import { ErrorFunction } from '@/error.service';
+import { PrismaService } from '../prisma.service';
+import { jwtConstants } from './constants';
+import { ErrorFunction } from '../error.service';
 // import { REQUEST } from '@nestjs/core';
 // import { Request } from 'express';
 
