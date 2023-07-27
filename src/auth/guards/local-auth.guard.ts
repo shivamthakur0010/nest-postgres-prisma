57b64d9655a2c104fe73eaffe6befa5e6d0110b0
shivamt <shivamt@wiznox.com>
2023-07-27 13:09:51 +0530

    5th test

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Global, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+@Global()
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
