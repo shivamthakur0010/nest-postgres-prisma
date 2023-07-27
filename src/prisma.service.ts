1e4124ef3eeec1cb5ca5c2faeecf7ee379543a73
shivamt <shivamt@wiznox.com>
2023-07-27 12:32:50 +0530

    3rd commit

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
+import { Injectable, OnModuleInit, Global } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+@Global()
 @Injectable()
 export class PrismaService extends PrismaClient
   implements OnModuleInit {
